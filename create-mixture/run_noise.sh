fd652613e2fd9eea128071dd051dff030aee4fa7
albert <imalbert@sogang.ac.kr>
2024-08-19 12:57:06 +0900

    run rir once

---
+++ create-mixture/run_noise.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+./matlab -nodisplay -nodesktop -r "cd /home/albert/Albert-Conv-TasNet/create-mixture/;run('gen_noise_rir.m'); exit;" &
+./matlab -nodisplay -nodesktop -r "cd /home/albert/Albert-Conv-TasNet/create-mixture/;run('gen_noise_rir.m'); exit;" &
+./matlab -nodisplay -nodesktop -r "cd /home/albert/Albert-Conv-TasNet/create-mixture/;run('gen_noise_rir.m'); exit;" &
+./matlab -nodisplay -nodesktop -r "cd /home/albert/Albert-Conv-TasNet/create-mixture/;run('gen_noise_rir.m'); exit;" &
+./matlab -nodisplay -nodesktop -r "cd /home/albert/Albert-Conv-TasNet/create-mixture/;run('gen_noise_rir.m'); exit;" &
+./matlab -nodisplay -nodesktop -r "cd /home/albert/Albert-Conv-TasNet/create-mixture/;run('gen_noise_rir.m'); exit;" &
+./matlab -nodisplay -nodesktop -r "cd /home/albert/Albert-Conv-TasNet/create-mixture/;run('gen_noise_rir.m'); exit;" &
+./matlab -nodisplay -nodesktop -r "cd /home/albert/Albert-Conv-TasNet/create-mixture/;run('gen_noise_rir.m'); exit;" &
+./matlab -nodisplay -nodesktop -r "cd /home/albert/Albert-Conv-TasNet/create-mixture/;run('gen_noise_rir.m'); exit;" &
+
+
+wait
+
+# 완료 메시지 출력
+echo "All MATLAB scripts have been executed."
