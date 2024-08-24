035ab98e3f4645822c0c2b4ead724ea5907c2239
albert_dev60 <bestjunh@gmail.com>
2024-08-24 09:47:38 +0900

    add shell script to gen whamr early

---
+++ whamr_scripts/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python create_wham_from_scratch.py --wsj0-root /home/nas/DB/wsj_wav/ --wham-noise-root /home/nas/user/Uihyeop/DB/wham_noise/ --output-dir /home/data/albert/DB/WHAMR_early_v2/
