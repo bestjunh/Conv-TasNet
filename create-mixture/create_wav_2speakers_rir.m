430411fcdbbab7730551862c59fc827a88ba2bad
albert <imalbert@sogang.ac.kr>
2024-08-29 01:36:04 +0900

    run with SNR

--- create-mixture/create_wav_2speakers_rir.m
+++ create-mixture/create_wav_2speakers_rir.m
@@ -32,13 +32,16 @@ end
 
 data_type = {'tr','cv','tt'};
 wsj0root = [nasPath 'DB/wsj_wav/']; % YOUR_PATH/, the folder containing wsj0/
+noiseroot = [nasPath 'user/Uihyeop/DB/wham_noise/'];
 rirroot = [devDataPath 'albert/DB/CONV-TASNet-RIR-v2/'];
-%output_dir16k=[devDataPath 'albert/DB/wsj0-mix_20240828_rir/2speakers/wav16k'];
-output_dir16k=['./tmp/'];
+output_dir16k=[devDataPath 'albert/DB/wsj0-mix_20240828_rir/2speakers_noise/wav16k/'];
+
 
 
 min_max = {'min'};%{'min','max'};
 upFs = 3*16000;
+nch = 1;
+NNoise = 18;
 RT60 = [0.2 0.3 0.4 0.5 0.6 0.7];
 
 ROOMDIM = {[4.01, 3.65, 2.81]
@@ -81,6 +84,9 @@ LOCDELTA = {[-0.52 -0.15,-0.11]
             [0.31 0.22 0.02]
             [0.27 0.43 -0.03]};
 
+SNRRange = [0 10];
+
+
 for i_mm = 1:length(min_max)
     for i_type = 1:length(data_type)
         if ~exist([output_dir16k '/' min_max{i_mm} '/' data_type{i_type}],'dir')
@@ -107,11 +113,13 @@ for i_mm = 1:length(min_max)
         
         num_files = length(C{1});
 
-        scaling_16k = zeros(num_files,2);
-        scaling_8k = zeros(num_files,2);
+        scaling_16k = zeros(num_files,2);        
         scaling16bit_16k = zeros(num_files,1);
-        scaling16bit_8k = zeros(num_files,1);
+        SNR_list = zeros(num_files,2);
+        
         fprintf(1,'%s\n',[min_max{i_mm} '_' data_type{i_type}]);
+
+        noiseWAVList = dir([noiseroot data_type{i_type}]);noiseWAVList = noiseWAVList(3:end);
         tic
         for i = 1:num_files
             disp(['Creating ' num2str(i_mm) '/' num2str(length(min_max)) ' ' num2str(i_type) '/' num2str(length(data_type)) ' ' num2str(i) '/' num2str(num_files)]);
@@ -137,30 +145,68 @@ for i_mm = 1:length(min_max)
             locDelta = cell2mat(LOCDELTA(micIdx));
             targetRIRPath = [rirroot 'target/RT' num2str(RT60(rtIdx)) '/ROOM' num2str(roomDim(1)) 'x' num2str(roomDim(2)) 'x' num2str(roomDim(3)) '/LOC' num2str(locDelta(1)) 'x' num2str(locDelta(2)) 'x' num2str(locDelta(3)) '/'];
             noiseRIRPath = [rirroot 'noise/RT' num2str(RT60(rtIdx)) '/ROOM' num2str(roomDim(1)) 'x' num2str(roomDim(2)) 'x' num2str(roomDim(3)) '/LOC' num2str(locDelta(1)) 'x' num2str(locDelta(2)) 'x' num2str(locDelta(3)) '/'];
-            rirList = dir(targetRIRPath);rirList = rirList(3:end);
+            target_rirList = dir(targetRIRPath);target_rirList = target_rirList(3:end);
+            noiseRIR = dir(noiseRIRPath);noiseRIR = noiseRIR(3:end);
+            
 
-            rir1Idx = randi(length(rirList));
+            rir1Idx = randi(length(target_rirList));
             while(1)
-                rir2Idx = randi(length(rirList));
+                rir2Idx = randi(length(target_rirList));
                 if rir1Idx ~= rir2Idx
                     break
                 end
             end
             
-            h = load([targetRIRPath rirList(rir1Idx).name]);
+            h = load([targetRIRPath target_rirList(rir1Idx).name]);
             h1 = h.h;
-            h = load([targetRIRPath rirList(rir2Idx).name]);
+            h = load([targetRIRPath target_rirList(rir2Idx).name]);
             h2 = h.h;
-            minLength = min(length(h1),length(h2));
+
+            h_noise = cell(1,18);
+            minLength = Inf;
+            maxVal = 0;
+            for src = 1:NNoise
+                htmp = load([noiseRIR(src).folder '/' noiseRIR(src).name]);
+                htmp = htmp.h;
+                h_noise{src} = htmp;
+                if minLength > length(htmp)
+                    minLength = length(htmp);
+                end
+                if maxVal < max(htmp)
+                    maxval = max(htmp);
+                end
+            end
+
+            minLength = min([length(h1),length(h2),minLength]);
             h1 = h1(1:minLength);
             h2 = h2(1:minLength);
-            maxVal = max(max(h1),max(h2));
+            maxVal = max([max(h1),max(h2),maxval]);
             h1 = h1/maxVal;
             h2 = h2/maxVal;
+            for src = 1:NNoise
+                h_noise{src} = h_noise{src}(1:minLength);
+                h_noise{src} = h_noise{src}/maxVal;
+            end
                         
             % get input wavs
             [s1, fs] = audioread([wsj0root C{1}{i}]);
             s2       = audioread([wsj0root C{3}{i}]);
+
+            cropWavLen = min(length(s1),length(s2));
+            noise = zeros(cropWavLen,NNoise);
+            for src = 1:NNoise
+                while(1)
+                    idx = randi(num_files);
+                    [tmp, fsNoise] = audioread([noiseroot data_type{i_type} '/' noiseWAVList(idx).name]);
+                    if length(tmp) < cropWavLen
+                        continue
+                    end
+                    startSample = randi(length(tmp)-cropWavLen)+1;
+                    noise(:,src) = tmp(startSample:startSample+cropWavLen-1,1);
+                    break
+                end
+                
+            end
             
             
             % Convolve with RIRs            
@@ -175,6 +221,14 @@ for i_mm = 1:length(min_max)
             s1_16k = resample(s1,16000,fs);
             s2_16k = resample(s2,16000,fs);
 
+            noise_up_rir = zeros(min(length(s1_up),length(s2_up)),NNoise);
+            noise_16k_rir = zeros(min(length(s1_16k),length(s2_16k)),NNoise);
+            for src = 1:NNoise
+                tmp_up = resample(noise(:,src),upFs,fs);
+                noise_up_rir(:,src) = tmp_up;
+                noise_16k_rir(:,src) = resample(tmp_up,fs,upFs);
+            end
+
             
             [s1_16k_rir,lev1]=activlev(s1_16k_rir,16000,'n'); % y_norm = y /sqrt(lev);            
             [s2_16k_rir,lev2]=activlev(s2_16k_rir,16000,'n');
@@ -186,6 +240,15 @@ for i_mm = 1:length(min_max)
             s2_16k_rir = weight_2 * s2_16k_rir / sqrt(lev2);
             s1_16k = weight_1 * s1_16k / sqrt(lev1);
             s2_16k = weight_2 * s2_16k / sqrt(lev2);
+
+            SNR = (SNRRange(2)-SNRRange(1))*rand+SNRRange(1);
+            energyTarget = sqrt(sum(abs(s1_16k_rir(1:length(noise_16k_rir))).^2));
+            energyInterference = sqrt(sum(sum(abs(noise_16k_rir).^2)));
+            energyNormal = energyTarget/energyInterference;
+            noise_16k_rir = noise_16k_rir*energyNormal/sqrt(10^(SNR/10));
+            % energyInterference = sqrt(sum(sum(abs(noise_16k).^2)));
+            % 20.*log10(energyTarget/energyInterference)
+
             
             switch min_max{i_mm}
                 case 'max'
@@ -204,7 +267,7 @@ for i_mm = 1:length(min_max)
                     s1_16k = cat(1,s1_16k,zeros(mix_16k_length - tmp_length,1));
                     s2_16k = cat(1,s2_16k,zeros(mix_16k_length - tmp_length,1));
             end
-            mix_16k = s1_16k_rir + s2_16k_rir;
+            mix_16k = s1_16k_rir + s2_16k_rir + sum(noise_16k_rir,2);
             
             max_amp_16k = max(cat(1,abs(mix_16k(:)),abs(s1_16k_rir(:)),abs(s2_16k_rir(:))));
             mix_scaling_16k = 1/max_amp_16k*0.9;
@@ -218,7 +281,8 @@ for i_mm = 1:length(min_max)
             % necessary scaling factors
             
             scaling_16k(i,1) = weight_1 * mix_scaling_16k/ sqrt(lev1);
-            scaling_16k(i,2) = weight_2 * mix_scaling_16k/ sqrt(lev2);            
+            scaling_16k(i,2) = weight_2 * mix_scaling_16k/ sqrt(lev2);      
+            SNR_list(i) = SNR;
             
             scaling16bit_16k(i) = mix_scaling_16k;
 
@@ -245,7 +309,7 @@ for i_mm = 1:length(min_max)
             % end
             
         end        
-        save([output_dir16k '/' min_max{i_mm} '/' data_type{i_type} '/scaling.mat'],'scaling_16k','scaling16bit_16k');
+        save([output_dir16k '/' min_max{i_mm} '/' data_type{i_type} '/scaling.mat'],'scaling_16k','scaling16bit_16k','SNR_list');
         
         fclose(fid);
         fclose(fid_s1);
